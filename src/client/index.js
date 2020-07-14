0768e2cf5607a2386f7e13d3b421f3f2920ff05a
Alaa Alhazmi <alaaalhazmi-is@hotmail.com>
2020-07-15 02:40:25 +0300

    Setting Up Page Layout

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,22 @@
 //import {} from './js/app.js';
 import "./styles/main.scss";
-import "@fortawesome/fontawesome-free/js/all.js"
+import "@fortawesome/fontawesome-free/js/all.js";
+import "./assets/imgs/madinah.jpg";
+
+/*function addCard(){
+	const allTripsDiv = document.querySelector('.allTrips');
+
+	const tripCardDiv = document.createElement('div');
+	tripCardDiv.className = "card col-4";
+	tripCardDiv.innerHTML = `
+		<img src="./src/client/assets/imgs/madinah.jpg" class="card-img-top" alt="...">
+		<div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+	`
+	allTripsDiv.appendChild(tripCardDiv)
+
+}
+
+addCard();*/
